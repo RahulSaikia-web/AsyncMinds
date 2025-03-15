aed7525b52ff28ac16bd54d50c246ead541cea5d
RahulSaikia-web <rahulsaikia682@gmail.com>
2025-03-15 23:23:28 +0530

    add bootstrap

--- src/pages/CompletedProjects.jsx
+++ src/pages/CompletedProjects.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Navbbar() {
+function CompletedProjects() {
   return (
     <div>
       
@@ -8,4 +8,4 @@ function Navbbar() {
   )
 }
 
-export default Navbbar
+export default CompletedProjects
